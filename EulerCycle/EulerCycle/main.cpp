b46c7589688998327ecd92d951ed642e474b93f3
Ido Raphaeli <ido010111@gmail.com>
2025-09-07 21:28:26 +0300

    Added method to measure and display algorithm runtime

--- EulerCycle/EulerCycle/main.cpp
+++ EulerCycle/EulerCycle/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include "Methods.h"
 #include "Euler.h"
 
@@ -15,15 +16,30 @@ int main() {
     if (e.IsEulerian()) {
 
         switch (Efficient) {
-        case 1:
+        case 1: {
+            auto start = chrono::high_resolution_clock::now();
             e.FindEulerCycle();
             e.PrintCycle();
+            auto end = chrono::high_resolution_clock::now();
+            auto duration =
+                std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            if (showRunTime) {
+                std::cout << "\nFunction took " << duration.count() << " microseconds." << std::endl;
+            }
             break;
-
-        case 2:
+        }
+        case 2: {
+            auto start = chrono::high_resolution_clock::now();
             e.FindEulerCycle();
             e.PrintCycle();
+            auto end = chrono::high_resolution_clock::now();
+            auto duration =
+                std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            if (showRunTime) {
+                std::cout << "\nFunction took " << duration.count() << " microseconds." << std::endl;
+            }
             break;
+        }
         }
     }
 }
